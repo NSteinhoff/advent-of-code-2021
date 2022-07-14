49c5cd005ef075d4e397c7ae194503a321ef1af1
Niko Steinhoff <niko.steinhoff@pm.me>
2022-07-14 08:47:16 +0200

    Comment 8-2.c

--- 8-2.c
+++ 8-2.c
@@ -11,34 +11,70 @@
 #define N_SIGNALS 10
 #define N_PERMUTATIONS 5040 // 7 segments mean 7! permutations
 
-const char segments[8] = "abcdefg";
+const char segments[SIGNAL_LENGTH] = "abcdefg";
+
+/// Lookup table of the original singal strings. Each numeric index maps to the
+/// corresponding signal.
 const char *signals[N_SIGNALS] = {
-    "abcefg", "cf",     "acdeg", "acdfg",   "bcdf",
-    "abdfg",  "abdefg", "acf",   "abcdefg", "abcdfg",
+	/* 0 */ "abcefg",
+	/* 1 */ "cf",
+	/* 2 */ "acdeg",
+	/* 3 */ "acdfg",
+	/* 4 */ "bcdf",
+	/* 5 */ "abdfg",
+	/* 6 */ "abdefg",
+	/* 7 */ "acf",
+	/* 8 */ "abcdefg",
+	/* 9 */ "abcdfg",
 };
 
-static char permutations[N_PERMUTATIONS][SIGNAL_LENGTH];
+/// Array of all permutations of the segments. It's constant, but we generate
+/// it each time at the start of `main`.
+char permutations[N_PERMUTATIONS][SIGNAL_LENGTH];
+size_t n_permutations = 0;
 
+/// Swap two characters in place.
 static inline void swap(char *s, size_t a, size_t b) {
+	if (a == b)
+		return;
 	char tmp = s[a];
 	s[a] = s[b];
 	s[b] = tmp;
 }
 
-static void permutate(char *pattern, size_t size, size_t n) {
-	static size_t count = 0;
-	if (size <= 1) {
-		strcpy(permutations[count++], pattern);
+/// Create all permutations of the first `n` characters of the input string.
+/// abc -> bac -> cab -> acb -> bca -> cba
+/// ^^     ^ ^    ^^     ^ ^    ^^
+void permutate(char *pattern, size_t n) {
+	if (n <= 1) {
+		strcpy(permutations[n_permutations++], pattern);
 		return;
 	}
 
-	for (size_t i = 0; i < size; i++) {
-		permutate(pattern, size - 1, n);
-		swap(pattern, size % 2 == 0 ? i : 0, size - 1);
+	for (size_t i = 0; i < n; i++) {
+		permutate(pattern, n - 1);
+		swap(pattern, n % 2 == 0 ? i : 0, n - 1);
 	}
 }
 
-static int pattern_to_signal(const char *pattern, const char *mapping) {
+/// Return the signal, if any, which can be decoded from the pattern using the
+/// mapping.
+///
+/// f         c          a
+/// fbcdeag fbcdeag fbcdeag
+/// abcdefg abcdefg abcdefg
+/// a         c          f   -> acf -> 7
+///
+/// d         e          a
+/// dbefcag dbefcag dbefcag
+/// abcdefg abcdefg abcdefg
+/// a         c          f   -> acf -> 7
+///
+///   e     d            a
+/// dbefcag dbefcag dbefcag
+/// abcdefg abcdefg abcdefg
+///   c     a            f   -> caf -> acf -> 7
+int pattern_to_signal(const char *pattern, const char *mapping) {
 	// What is the length of the incoming pattern
 	size_t len = strlen(pattern);
 
@@ -77,8 +113,8 @@ static int pattern_to_signal(const char *pattern, const char *mapping) {
 	return -1;
 }
 
-static bool matches_all_signals(const char *mapping,
-                                char patterns[][SIGNAL_LENGTH]) {
+/// Check if the mapping can decode all signals
+bool matches_all_signals(const char *mapping, char patterns[][SIGNAL_LENGTH]) {
 	int matches[N_SIGNALS] = {0};
 
 	for (int j = 0; j < N_SIGNALS; j++) {
@@ -103,20 +139,23 @@ int main(void) {
 
 	// First we generate all the possible ways that the signal wires might
 	// be scrambled, which means all the permutations of the character
-	// sequence "abcdfg".
-	char tmp[8];
-	permutate(strcpy(tmp, "abcdefg"), 7, 7);
+	// sequence of segments. There are 7! of those, so we generate them.
+	// `permutate` is not pure, so we need to pass it its own copy of the
+	// segments.
+	char *tmp = strdup(segments);
+	permutate(tmp, N_SEGMENTS);
+	free(tmp);
 
 	char in[N_SIGNALS][SIGNAL_LENGTH];
 	char out[N_OUTPUTS][SIGNAL_LENGTH];
 
 	// For each line of input patterns, we find the permutation which
-	// uniquely decodes all the patterns into signals.
+	// uniquely decodes all the patterns into the unique signals.
 	// Using this permutation, we then decode the output patterns.
 	int sum_of_outputs = 0;
 	while (fscanf(file, "%s %s %s %s %s %s %s %s %s %s | %s %s %s %s",
-	              in[0], in[1], in[2], in[3], in[4], in[5], in[6], in[7],
-	              in[8], in[9], out[0], out[1], out[2], out[3]) != EOF) {
+		      in[0], in[1], in[2], in[3], in[4], in[5], in[6], in[7],
+		      in[8], in[9], out[0], out[1], out[2], out[3]) != EOF) {
 		// Go through all the possible permutations to find one that can
 		// match all 10 signals in the given input.
 		for (int i = 0; i < N_PERMUTATIONS; i++) {
@@ -126,10 +165,10 @@ int main(void) {
 				// start decoding the output patterns.
 				int output = 0;
 				for (int j = 0; j < N_OUTPUTS; j++) {
-					int signal =
-					    pattern_to_signal(out[j], mapping);
-					output +=
-					    signal * pow(10, N_OUTPUTS - j - 1);
+					int signal = pattern_to_signal(out[j],
+								       mapping);
+					output += signal *
+						  pow(10, N_OUTPUTS - j - 1);
 				}
 				sum_of_outputs += output;
 
